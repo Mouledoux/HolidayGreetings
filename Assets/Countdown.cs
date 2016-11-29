518327092684b776dc173567af590635885f0bed
Zac King <ZacKingx@gmail.com>
2016-11-29 17:25:48 -0600

    integrating new assets

---
+++ Assets/Countdown.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Countdown : MonoBehaviour
+{
+    Text text;
+    RectTransform rect;
+
+    public UnityEvent loadedAction;
+
+    void Start()
+    {
+        text = gameObject.GetComponent<Text>();
+        rect = gameObject.GetComponent<RectTransform>();
+    }
+
+    [ContextMenu("Do the thing")]
+    public void StartCount()
+    {
+        StartCoroutine(StartCountDown());
+    }
+
+    IEnumerator StartCountDown()
+    {
+        float timer = 4;
+
+        while(timer > 0)
+        {
+            timer -= Time.deltaTime;
+
+            rect.localScale = new Vector3(timer % 1 * 5, timer % 1 * 5, 1);
+
+            if (timer > 1)
+                text.text = ((int)timer).ToString();
+            else
+                text.text = "GO!";
+            yield return null;
+        }
+
+        loadedAction.Invoke();
+    }
+}
