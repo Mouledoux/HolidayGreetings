a1519f2017e328ab11a2eb0ec7940f37fa481aa2
Zac King <ZacKingx@gmail.com>
2016-12-01 20:33:04 -0600

    changed trees

--- Assets/ChangeListMaterial.cs
+++ Assets/ChangeListMaterial.cs
@@ -8,12 +8,28 @@ public class ChangeListMaterial : MonoBehaviour
     public Material mat;
     public List<GameObject> listToChange;
 
+    public string grabName;
+
     [ContextMenu("Change Materials")]
     public void ChangeMaterials()
     {
         foreach(GameObject g in listToChange)
         {
             g.GetComponent<Renderer>().material = mat;
+        }
+    }
+
+    [ContextMenu("GrabAll")]
+    public void grabAll()
+    {
+        var v = GameObject.FindObjectsOfType<GameObject>();
+
+        foreach(GameObject g in v)
+        {
+            if (g.name == grabName)
+            {
+                listToChange.Add(g);
+            }
         }
     }
 }
