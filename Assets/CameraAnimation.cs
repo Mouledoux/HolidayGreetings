443a69bddd8bfa1c278829f6f8f080fc3f170fed
Zac King <ZacKingx@gmail.com>
2016-12-01 20:13:15 -0600

    changes to enviroment and animations

---
+++ Assets/CameraAnimation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraAnimation : MonoBehaviour
+{
+    Animation anim;
+    public AnimationClip vr;
+    public AnimationClip full;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animation>();
+    }
+
+    public void PlayVR()
+    {
+
+        
+    }
+
+    public void PlayFull()
+    {
+        anim.Play(full.name);
+    }
+}
