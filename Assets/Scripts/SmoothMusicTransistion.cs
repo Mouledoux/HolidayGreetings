dfe2e19b2434d5549883b387fb1b3a2b24d8dfed
Eric Mouledoux <EricMouledoux@gmail.com>
2016-12-06 10:28:43 -0600

    Script for smooth audio

---
+++ Assets/Scripts/SmoothMusicTransistion.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Description:
+/// Used to smoothly transistion between LOOPING audio.
+/// </summary>
+public class SmoothMusicTransistion : MonoBehaviour
+{
+    /// <summary>
+    /// Ensures there is an audio source on the object, and that looping is set to FALSE
+    /// </summary>
+    void Awake()
+    {
+        if(!audioSource)
+        {
+            audioSource = GetComponent<AudioSource>() ?
+                GetComponent<AudioSource>() : gameObject.AddComponent<AudioSource>() as AudioSource;
+        }
+
+        audioSource.loop = false;
+
+        m_currentTrack = m_nextTrack = audioTracks[0];
+
+        StartCoroutine(PlayAudioClips());
+    }
+
+    public void SetNextTrack(int trackIndex)
+    {
+        m_nextTrack = audioTracks[trackIndex];
+    }
+
+    //public void SetNextTrack(AudioClip newClip)
+    //{
+    //    int trackIndex;
+
+    //    if (!audioTracks.Contains(newClip))
+    //    {
+    //        audioTracks.Add(newClip);
+    //    }
+
+    //    trackIndex = audioTracks.IndexOf(newClip);
+    //    SetNextTrack(trackIndex);
+    //}
+
+    [ContextMenu("Next")]
+    public void NextTrack()
+    {
+        SetNextTrack(1);
+    }
+
+    public IEnumerator PlayAudioClips()
+    {
+        while (true)
+        {
+            audioSource.clip = m_currentTrack;
+            audioSource.Play();
+            yield return new WaitWhile(() => audioSource.isPlaying);
+            m_currentTrack = m_nextTrack;
+        }
+    }
+
+    public AudioSource audioSource;
+    public List<AudioClip> audioTracks = new List<AudioClip>();
+
+    private AudioClip m_currentTrack;
+    private AudioClip m_nextTrack;
+}
