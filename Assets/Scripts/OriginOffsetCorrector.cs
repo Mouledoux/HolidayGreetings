daab0d987458cc923d3808c67c1cf456c1b4f39f
Eric Mouledoux <EricMouledoux@gmail.com>
2016-12-07 10:10:09 -0600

    Script for correcting yetti lights

---
+++ Assets/Scripts/OriginOffsetCorrector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class OriginOffsetCorrector : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
