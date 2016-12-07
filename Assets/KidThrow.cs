c084a91393272ecfb2b959dd87fa8af83f6857fa
Zac King <ZacKingx@gmail.com>
2016-12-07 16:13:39 -0600

    eod push

---
+++ Assets/KidThrow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KidThrow : MonoBehaviour
+{
+    Animator anim;
+    public Animation animation;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        anim.SetTrigger("Throw");
+    }
+
+    [ContextMenu("throw")]
+    public void AnimateSnowBall()
+    {
+        animation.Play();
+    }
+}
---
+++ Assets/Scripts/DelayAnimation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayAnimation : MonoBehaviour
+{
+    public float delay = 1;
+    public GameObject go;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(AnimDelay());
+    }
+
+    IEnumerator AnimDelay()
+    {
+        float timer = 0;
+
+        while (timer <= delay)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        go.SetActive(true);
+    }
+}
