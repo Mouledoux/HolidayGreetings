43f82a438c1c80f3c84d0d038c8135365cf5f77e
Eric Mouledoux <EricMouledoux@gmail.com>
2016-12-09 09:38:13 -0600

    Build 26

---
+++ Assets/Scripts/OnTriggerAudio.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnTriggerAudio : MonoBehaviour
+{
+    public string tag = "";
+    public AudioSource audioSource;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(tag != "")
+        {
+            if(!other.gameObject.CompareTag(tag))
+            {
+                return;
+            }
+        }
+        audioSource.Play();
+    }
+}
